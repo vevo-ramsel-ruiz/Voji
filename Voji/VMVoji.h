e9666ad93f4408d9a51bdb4bffde4920c6ee035d
ramsel <ramsel.ruiz@vevo.com>
2015-08-17 15:39:45 -0700

    added pch

---
+++ Voji/VMVoji.h
@@ -0,0 +1,13 @@
+//
+//  VMVoji.h
+//  Voji
+//
+//  Created by Ramsel J Ruiz on 8/17/15.
+//  Copyright (c) 2015 Vevo. All rights reserved.
+//
+
+#import "PFObject.h"
+
+@interface VMVoji : PFObject
+
+@end
---
+++ Voji/VMVoji.m
@@ -0,0 +1,13 @@
+//
+//  VMVoji.m
+//  Voji
+//
+//  Created by Ramsel J Ruiz on 8/17/15.
+//  Copyright (c) 2015 Vevo. All rights reserved.
+//
+
+#import "VMVoji.h"
+
+@implementation VMVoji
+
+@end
